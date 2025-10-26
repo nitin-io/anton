556e8c010c8f35f6edbe92a4c29701760656844c
Nitin Chaudhary <nitin.io@outlook.com>
2025-10-26 15:08:23 +0530

    added verbose option

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
+
+verbose = False
+user_prompt = ""
+
+if "--verbose" in sys.argv:
+    verbose = True
+    sys.argv = list(filter(lambda arg: arg != "--verbose", sys.argv))
+
+user_prompt = sys.argv[1]
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,17 +19,21 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 try:
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=[sys.argv[1]]
+        contents=messages
     )
+    # GenerateContentResponse
+    print(f"GenAI Response: {response.text}")
+    # print(response.usage_metadata)
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 except IndexError:
     print("prompt not provied")
     exit(1)
-
-# GenerateContentResponse
-print(response.text)
-# print(response.usage_metadata)
-
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
