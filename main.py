097a4f509814000c8789ed80c817992670c2fad7
Nitin Chaudhary <nitin.io@outlook.com>
2025-10-26 12:05:48 +0530

    added prompt to recevied in argument"

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=["Why one should learn python? Use one paragraph maximum."]
-)
+try:
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=[sys.argv[1]]
+    )
+except IndexError:
+    print("prompt not provied")
+    exit(1)
 
 # GenerateContentResponse
 print(response.text)
