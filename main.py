2db43537bb5809851e541248f648ff858492dd98
Nitin Chaudhary <nitin.io@outlook.com>
2025-10-26 10:00:38 +0530

    added basic genai setup

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=["Why one should learn python? Use one paragraph maximum."]
+)
+
+# GenerateContentResponse
+print(response.text)
+# print(response.usage_metadata)
+
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
